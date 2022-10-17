b004becde4c8f1fa724af438216aeb3bbc89a541
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2022-10-17 14:23:28 +0200

    comparison output

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	ics "github.com/arran4/golang-ical"
@@ -11,7 +12,7 @@ var version = "0.1.0"
 
 func main() {
 	log.Info("Welcome to ical-tools, version " + version)
-	log.SetLevel(log.DebugLevel)
+	log.SetLevel(log.InfoLevel)
 	log.Debug("Debug log is enabled") // only shows if Debug is actually enabled
 
 	//start module from os.Args[1]
@@ -24,5 +25,19 @@ func main() {
 	file2, _ := os.Open("./ical-2.ics")
 	calendar2, _ = ics.ParseCalendar(file2)
 
-	compare(calendar1, calendar2)
+	added, deleted, changed := compare(calendar1, calendar2)
+
+	log.Debug("Added: ", len(added))
+	log.Debug("Deleted: ", len(deleted))
+	log.Debug("Changed: ", len(changed))
+
+	for _, event := range added {
+		fmt.Println("Added:\n\n", event.Serialize())
+	}
+	for _, event := range deleted {
+		fmt.Println("Deleted:\n\n", event.Serialize())
+	}
+	for _, event := range changed {
+		fmt.Println("Changed:\n\n", event.Serialize())
+	}
 }
